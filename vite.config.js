37c1c9930aaf29b2409b6484d6a5ae5638932f12
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-02-19 15:26:47 -0300

    adicionando o host

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.16.80',
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://192.168.16.80:8000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      }
-    },
     hmr: {
       host: '192.168.16.80',
       port: 5173,
     }
   },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
 });
