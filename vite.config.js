eac1323909741388fc573aa0e7d4eb1bc9c621e8
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-02-19 16:40:24 -0300

    ajustes server

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Permite que o servidor seja acessível externamente
-    port: 5173, // Porta padrão do Vite
-    hmr: {
-      host: '192.168.16.80',
-      port: 5173,
-    }
-  },
+  // server: {
+  //   host: '0.0.0.0', // Permite que o servidor seja acessível externamente
+  //   port: 5173, // Porta padrão do Vite
+  //   hmr: {
+  //     host: '192.168.16.80',
+  //     port: 5173,
+  //   }
+  // },
 });
