ad9c17b28fd3f1839dc4f945e4c70d21ace2bf56
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-02-19 15:29:40 -0300

    adicionando o host

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Permite que o servidor seja acessível externamente
+    port: 5173, // Porta padrão do Vite
+    proxy: {
+      '/api': {
+        target: 'http://192.168.16.80:8000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    },
     hmr: {
       host: '192.168.16.80',
       port: 80,
