6a03888840089d9ca155a13abb3c6067fb107a70
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-02-20 08:53:57 -0300

    ajustes path do proxy

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,13 @@ export default defineConfig({
     host: "0.0.0.0", // Permite acesso externo (caso rode em Docker ou outro IP)
     port: 5173,
     strictPort: true, // Garante que use a porta 5173
-    hmr: {
-      clientPort: 5173, // Se rodar em Docker ou WSL, pode precisar definir manualmente
-    },
     proxy: {
       "/api": {
-        target: "http://192.168.16.80:8000",
+        // target: "http://192.168.16.80:8000",
+        target: "http://192.168.16.13:8000",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace('/api', ""),
       },
     },
   },
